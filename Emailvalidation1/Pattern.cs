862b37ca33536e892775aecda588d7dfef1748c5
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-12 20:32:21 +0530

    [Shreya] Validating the email

---
+++ Emailvalidation1/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailValidation1
+{
+    class Pattern
+    {
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+        public bool ValidateEmail(string mail)
+        {
+            return Regex.IsMatch(mail, EMAIL_REGEX);
+        }
+    }
+}
---
+++ Emailvalidation1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace EmailValidation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pin = new Pattern();
+            bool validResult = pin.ValidateEmail("abc@yahoo.com.com");
+           Console.WriteLine("Email validation:"+" "+validResult);
+        }
+    }
+}
